c713dcf9be6e4cbc1c10d0c6e68a1a7624916a68
Ryan Bui <z5421437@ad.unsw.edu.au>
2026-02-07 14:48:05 +1100

    Organised basic home page layout

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Will It Get Binned</h1>
+      <div className="card">
+        <label>
+          Flight Number:{" "}
+          <input type="text" />
+        </label>
+      </div>
+    </>
+  )
+}
+
+export default App
