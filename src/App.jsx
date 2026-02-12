a19b825ee7649198359c10f56ef8a8b8246cdc3b
Ryan Bui <z5421437@ad.unsw.edu.au>
2026-02-12 13:14:18 +1100

    Added light and dark theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [flightNumber, setFlightNumber] = useState("");
+  const [activeMode, setActiveMode] = useState('light');
 
   function handleFlightChange(e) {
     setFlightNumber(e.target.value);
   }
 
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (activeMode === 'dark') {
+      body.className = 'dark';
+    } else {
+      body.className = 'light';
+    }
+  }, [activeMode]);
+
   return (
-    <>
+    <div>
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Will It Get Binned</h1>
-      <div className="card">
-        <label>
-          Flight Number:{" "}
-          <input type="text" onChange={handleFlightChange} />
+      <h1>Will It Get Binned?</h1>
+      <div>
+        <label className="flight-card">
+          Flight Number
+          <input id="flight-input" type="text" onChange={handleFlightChange} />
         </label>
       </div>
       <p>{flightNumber}</p>
-    </>
+      <label aria-label="Toggle Light/Dark Mode" className="switch">
+        <input type="checkbox" onChange={() => {
+          activeMode === 'light' ? setActiveMode('dark') : setActiveMode('light');
+        }} />
+        <span className="slider"></span>
+      </label>
+    </div>
   )
 }
 
